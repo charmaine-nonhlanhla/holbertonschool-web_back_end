b29d99e25707c8e5f828c8b8eb3a0d37766bb512
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2025-01-14 06:38:41 +0000

    12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
 export default function createReportObject(employeesList) {
-  // Create the report object
   return {
-    allEmployees: { ...employeesList },  // Use spread to copy the employees list
-    getNumberOfDepartments: function() {
-      // Return the number of departments by counting the keys of employeesList
-      return Object.keys(employeesList).length;
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
     },
   };
 }
